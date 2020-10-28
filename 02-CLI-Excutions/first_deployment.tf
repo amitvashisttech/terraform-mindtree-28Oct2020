17b0d6f4c4c0eb9cbb60d6e7f919916aeddc2bcb
Amit Vashist <amitvashist7@outlook.com>
2020-10-28 07:14:26 +0000

    02-CLI-Excutions

---
+++ 02-CLI-Excutions/first_deployment.tf
@@ -0,0 +1,14 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0947d2ba12ee1ff75"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld-AV"
+  }
+}
